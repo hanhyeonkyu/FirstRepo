3353739be975c793cc8f5221bdf3092c5b71c92a
hanhyeonkyu <busgod1212@gmail.com>
2019-04-13 12:26:26 +0900

    initial commit

---
+++ hello.js
@@ -0,0 +1,11 @@
+import react from 'react';
+
+class hello extends React.Component{
+    render(){
+        return(
+            <div>
+                hello^^
+            </div>
+        )
+    }
+}
