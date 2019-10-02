ccab398e2ad7c7083222dc5d94a9385327edfa9c
hanhyeonkyu <46094938+hanhyeonkyu@users.noreply.github.com>
2019-10-02 14:53:49 +0900

    Update hello.js

--- hello.js
+++ hello.js
@@ -4,7 +4,9 @@ class hello extends React.Component{
     render(){
         return(
             <div>
-                Git init - commit(with comment) - fetch - pull -push
+                Git init - commit(with comment) 
+                 git remote add origin [url]
+            - fetch - pull - push
                 git pull test - Success!
             </div>
         )
